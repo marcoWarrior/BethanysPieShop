af40f3697192030715ab95f75025451655ffca34
Marco <marco.guerriero1994@gmail.com>
2024-10-07 11:41:37 +0200

    Creato componente Models
    Creazione del componente Models del pattern MVC.
    
    Sono stati creati modelli, interfacce repository e Mocks (classi che simulano l'estrazione dei dati dal DataBase).
    
    Infine è stata effettuata una DI "Dependency Injecton" dei due servizi creati in AddScoped

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,21 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddControllersWithViews(); // Add MVC services
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // Middleware component: Enable static files
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // Middleware component: Enable developer exception page
+}
+
+app.MapDefaultControllerRoute(); // Middleware component: Enable default controller route, richieste in arrivo
 
 app.Run();
