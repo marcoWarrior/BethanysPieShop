aa71eb28284a294bacab8e1ef19f1c80dc5d0a5e
Marco <marco.guerriero1994@gmail.com>
2024-10-10 11:24:47 +0200

    Migrazione, configurazione e creazione del DB

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,11 +1,16 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 
 builder.Services.AddControllersWithViews(); // Add MVC services
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 
@@ -17,5 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute(); // Middleware component: Enable default controller route, richieste in arrivo
+DbInitializer.Seed(app); // Seed the database
 
 app.Run();
