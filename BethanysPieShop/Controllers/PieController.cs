438b50f7334648b233ebde3b22bf04d417b2bbb9
Marco <marco.guerriero1994@gmail.com>
2024-10-17 11:45:21 +0200

    Fixed and add Views Components
    Cambiato l'import di Bootstrap aggiungendo il bundle in quanto non mi funzionava il dropdown della nav bar "SHOP"
    
    Aggiunto View Components come SHOP e il carrellino, ora saranno presenti in tutte le views.

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -9,28 +9,48 @@ namespace BethanysPieShop.Controllers
         private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
 
-        //Constructor
         public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
-        {
-            //ViewBag.CurrentCategory = "Cheese cakes";
-            //return View(_pieRepository.AllPies);
-            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All pies");
-            return View(pieListViewModel);
+        //public IActionResult List()
+        //{
+        //    //ViewBag.CurrentCategory = "Cheese cakes";
 
+        //    //return View(_pieRepository.AllPies);
+
+        //    PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+        //    return View(piesListViewModel);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
-        public IActionResult Details(int id) {
+        public IActionResult Details(int id)
+        {
             var pie = _pieRepository.GetPieById(id);
             if (pie == null)
-            {
                 return NotFound();
-            }
+
             return View(pie);
         }
     }
